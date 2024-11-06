f49f56727bcc7f1ee63964a8c9f0e05ce5c3ee8c
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2024-11-06 17:10:44 -0300

    adicionado função para fechar o menu mobile quando o item do menu for pressionado

--- script/script.js
+++ script/script.js
@@ -1,11 +1,39 @@
 function slideMenu() {
+    let menuButton = document.querySelector(".nav-ul a.icon");
     let menuList = document.querySelector(".div-nav-li-b");
+    let menuNav = document.querySelector(".nav");
+    let menuItem = document.querySelectorAll(".menu-item");
+
+    document.addEventListener('click', (event) => {
+        for (let index = 0; index < menuItem.length; index++) {
+            const element = menuItem[index];
+            if (element.contains(event.target) == true){
+                menuNav.style.backgroundColor = "#ffffff45"
+                menuNav.style.boxShadow = ""
+                menuList.style.height = "0px"
+                menuList.style.padding = "0rem 0rem 0rem"
+            }
+            
+        }
+    });
+
     if (menuList.style.height === "0px") {
-    menuList.style.height = "280px"
-    menuList.style.padding = "1rem 0rem 0rem"
+        menuNav.style.backgroundColor = "#ED5E11"
+        menuNav.style.boxShadow = "#000000 0 0px 20px"
+        menuList.style.height = "280px"
+        menuList.style.padding = "1rem 0rem 0rem"
     } else {
+        menuNav.style.backgroundColor = "#ffffff45"
+        menuNav.style.boxShadow = ""
         menuList.style.height = "0px"
         menuList.style.padding = "0rem 0rem 0rem"
     }
-
-  };
+    document.addEventListener('click', (event) => {
+        if (!menuList.contains(event.target) && !menuButton.contains(event.target)) {
+            menuNav.style.backgroundColor = "#ffffff45"
+            menuNav.style.boxShadow = ""
+            menuList.style.height = "0px"
+            menuList.style.padding = "0rem 0rem 0rem"
+        }
+    });
+};
